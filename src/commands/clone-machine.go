e16ba98851e732c389099070bcd6c7589116815d
Gabriel Lemire <gabriel.lemire@gmail.com>
2024-02-03 02:53:34 -0500

    Attempt at fixing MAC addr issue on clone

--- src/commands/clone-machine.go
+++ src/commands/clone-machine.go
@@ -1,6 +1,8 @@
 package commands
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type CloneMachineArgs struct {
 	SourceMachineID string
@@ -8,10 +10,20 @@ type CloneMachineArgs struct {
 }
 
 func (c *Commander) CloneMachine(args CloneMachineArgs) (MachineDef, error) {
-	_, err := c.exec([]string{"clone", args.SourceMachineID, "--name", args.NewMachineName})
+	out, err := c.exec([]string{"clone", args.SourceMachineID, "--name", args.NewMachineName})
 	if err != nil {
 		return MachineDef{}, fmt.Errorf("error cloning machine: %w", err)
 	}
+
+	// If there is any output, it's likely an error from stderr
+	if out != "" {
+		return MachineDef{}, fmt.Errorf("error cloning machine: %s", out)
+	}
+
+	// There is an issue where we clone a machine and it won't boot at the same time
+	// as other instances from the same template. This is because the MAC address is the same.
+	// Even changing the MAC address in the plist file doesn't seem to fix it, since UTM likely
+	// caches the value of the field.
 
 	machine, err := c.GetMachineByName(args.NewMachineName)
 	if err != nil {
